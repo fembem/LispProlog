c30c69951dc4e25f4f37b090eda6a42f96d4631b
Leo <leo.decandia@gmail.com>
2012-04-07 12:19:38 -1000

    some changes to assign_first_task function.

--- hw6/hw6.pl
+++ hw6/hw6.pl
@@ -19,10 +19,10 @@
 assign_first_task([Task_name, Task_class, Task_time], 						%the first task
 			[ [Person_name, Time_left, Person_skills] | Other_people ], 	%the people
 			Assignments,													%the present assignments
-			New_assignments,											    %the new assignments
-			New_people) :-													%the 'new' people list
+			New_people,											    		%the new assignments
+			New_assignments) :-												%the 'new' people list
 																			%the first person has less time left
-    			member(Task_class, Person_name), 
+    			member(Task_class, Person_skills), 
     			Task_time < Time_left, 
     			New_time_left is Time_left - Task_time,
     			%same partial people list as before but first person on the list has less time left
@@ -30,8 +30,9 @@ assign_first_task([Task_name, Task_class, Task_time], 						%the first task
     			New_assignments is [ [Task_name, Person_name] | Assignments ].
 
 %assign the first task to someone other than the first person in the people list
-assign_first_task(Task, [_|Other_people], Assignments) :-
-    assign_first_task(Task, Other_people, Assignments).
+assign_first_task(Task, [Unsuitable_Person|Other_people], Assignments, New_people, New_assignments) :-
+    New_people is [Unsuitable_Person | Other_people],
+    assign_first_task(Task, Other_people, Assignments, [Unsuitable_Person | Other_people], New_assignments).
     			
 
 %when there are no tasks left to assign, there are no futher assignments that need to be made
@@ -39,10 +40,8 @@ constrain([], _, _).
 %we can find a satisfying assignment if we can assign the first task, and then find
 %a satisfying assignment for the other tasks to the people, of which the one assigned to the first task
 %has his/her time available decreased by the time the first task took
-constrain([ Task | Other_tasks], 
-			[ Person | Other_people], 
-			Assignments ) :-
-    			assign_first_task(Task, [Person|Other_people], Assignments, New_assignments, New_people),
+constrain([ Task | Other_tasks], People, Assignments ) :-
+    			assign_first_task(Task, People, Assignments, New_people, New_assignments),
     			constrain(Other_tasks, New_people, New_assignments ).
     			
 test1(Result) :-
