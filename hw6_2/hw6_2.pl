984d9a96d82ee28faa5ef4ed0d57b7d3548866c4
Leo <leo.decandia@gmail.com>
2012-04-12 23:50:58 -1000

    now giving all solutions, not just first.

--- hw6_2/hw6_2.pl
+++ hw6_2/hw6_2.pl
@@ -20,63 +20,55 @@
 %
 
 %utility functions for printing nested arrays 2 level deep
-printAssignments(Assignments) :- write('{'), printAssignmentsRec(Assignments).
-printAssignmentsRec([Assignment | []]) :- write(Assignment), write('}').
-printAssignmentsRec([Assignment | OtherAssignments]) :- write(Assignment), write(','), 
-														printAssignmentsRec(OtherAssignments).
+printAssignments2(Assignments) :- write('{'), printAssignmentsRec2(Assignments).
+printAssignmentsRec2([Assignment | []]) :- write(Assignment), write('}').
+printAssignmentsRec2([Assignment | OtherAssignments]) :- write(Assignment), write(','), 
+														printAssignmentsRec2(OtherAssignments).
 
 
 %assign the first task to the first person in the people list
-assign_first_task([Task_name, Task_class, Task_time], 							%the first task
-			[ [Person_name, Time_left, Person_skills] | Other_people ], 		%the people
-			[ [Person_name, New_time_left, Person_skills] | Other_people ], 	%the 'new' people
-			[Task_name, Person_name]											%the assignment
-			) :-
+assign_first_task2([Task_name, Task_class, Task_time], 						%the first task
+			[ [Person_name, Time_left, Person_skills] | Other_people ], 	%the people
+			Assignments,													%the present assignments
+			New_people,											    		%the new assignments
+			New_assignments) :-												%the 'new' people list
+																			%the first person has less time left
     			%write('consider assigning '), write(Task_name), write(' to '), write(Person_name), nl,
     			member(Task_class, Person_skills), 
     			Task_time =< Time_left,
     			%write('assigned '), write(Task_name), write(' to '), write(Person_name), nl,
-    			New_time_left is Time_left - Task_time.
+    			New_time_left is Time_left - Task_time,
     			%same partial people list as before but first person on the list has less time left
+    			New_person = [Person_name, New_time_left, Person_skills],
+    			New_people = [ New_person | Other_people ],
+    			New_assignments = [ [Task_name, Person_name] | Assignments ].
+    			%write('New_people '), write(New_people), nl.
+    			%write('Assignments '), printAssignments(Assignments), nl,
+    			%write('New_assignments '), printAssignments(New_assignments), nl.
 
 %assign the first task to someone other than the first person in the people list
-assign_first_task(Task, [Unsuitable_Person|Other_people], New_people, Assignment) :-
-    assign_first_task(Task, Other_people, New_people_2, Assignment_2),
+assign_first_task2(Task, [Unsuitable_Person|Other_people], Assignments, New_people, New_assignments) :-
+    assign_first_task2(Task, Other_people, Assignments, New_people_2, New_assignments_2),
     New_people = [Unsuitable_Person | New_people_2],
-    Assignment = Assignment_2.
+    New_assignments = New_assignments_2.
     			
 
 %when there are no tasks left to assign, there are no futher assignments that need to be made
-constrain([], People, [])
-						:- write('success!! '), nl,
-						write('Final people: '), write(People), nl.
+constrain2([], People, Assignments) :- write('success!! '), nl,
+						write('Final people: '), write(People), nl,
+						write('Final assignments: '), printAssignments2(Assignments), nl.
 %we can find a satisfying assignment if we can assign the first task, and then find
 %a satisfying assignment for the other tasks to the people, of which the one assigned to the first task
 %has his/her time available decreased by the time the first task took
-constrain([ Task | Other_tasks], People, [Assignment | Other_Assignments] ) :-
-    			assign_first_task(Task, People, New_people, Assignment),
-    			constrain(Other_tasks, New_people, Other_Assignments ).
-
-%test that assign_first_task works correctly
-test_assign_first_task_1(New_People, Assignment) :-
-	assign_first_task([frame, woodworking, 200],
-			[[mary, 300,[woodworking, concrete]], [john, 50, [concrete]]],
-			New_People,
-			Assignment
-			).
-%test that assign_first_task works correctly
-test_assign_first_task_2(New_People, Assignment) :-
-	assign_first_task([pour, concrete, 25],
-			[[mary, 300,[woodworking, concrete]], [john, 50, [concrete]]],
-			New_People,
-			Assignment
-			).
-
-test1(Result) :-
+constrain2([ Task | Other_tasks], People, Assignments ) :-
+    			assign_first_task2(Task, People, Assignments, New_people, New_assignments),
+    			constrain2(Other_tasks, New_people, New_assignments ).
+			
+test1_2(Result) :-
   constrain([[t1,a,5],[t2,b,10],[t3,c,15],[t4,c,10],[t5,a,15],[t6,b,10]],
             [[p1,20,[a,c]],[p2,10,[a,b]],[p3,15,[b]],[p4,30,[c]]],
 	    Result).
-test2(Result) :-
+test2_2(Result) :-
   constrain([[frame, woodworking, 200], [pour, concrete, 25], [wall, woodworking, 100]],
             [[mary, 300,[woodworking, concrete]], [john, 50, [concrete]]],
 	    Result).
