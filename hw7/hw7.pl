3260f9ccb29ffa36563046afe3b5c7f9008eae80
Leo <leo.decandia@gmail.com>
2012-04-22 09:30:03 -1000

    submission version with nested printing of planning.

--- hw7/hw7.pl
+++ hw7/hw7.pl
@@ -7,39 +7,41 @@
 
 %this is the base case, the goal predicates are satisfied at the current state 
 %so return an empty list as the plan, ie a null plan
-me_plan(Start, Goal, _, [], Start) :- 	%when we reach the goal the final end state will be our present 'start'
+me_plan(Start, Goal, _, [], Start, Depth) :- 	%when we reach the goal the final end state will be our present 'start'
 	subset(Goal, Start), !, %only look for the first solution
-	write('Goal '), print_stack(Goal), 
+	printDepth(Depth), write('Goal '), print_stack(Goal), 
     write('a subset of Start '), print_stack(Start), nl.
 
 %this is the case where we pick a move to achieve a predicate in the goal
 %but not satisfied in the current state, and we have the preconditions to take that move
-me_plan(Start, Goal, Been_list, [Name|Plan], End_state) :-
-    write('plan from '), print_stack(Start), 
+me_plan(Start, Goal, Been_list, [Name|Plan], End_state, Depth) :-
+    New_depth is Depth + 1,
+    printDepth(Depth), write('plan from '), print_stack(Start), 
     write(' to '), print_stack(Goal), nl,
     set_diff(Goal, Start, Diff_set),
-    write('Diff_set = '), print_stack(Diff_set), nl,
+    printDepth(Depth), write('Diff_set = '), print_stack(Diff_set), nl,
     member(Diff, Diff_set),				%just find first predicate we are missing
     %write('Diff = '), write(Diff), nl,
     move(Name, Preconditions, Actions),
     member(add(Diff), Actions), 		%more than one postcondition matching doesn't help
-    write(add(Diff)), write(' in actions'), nl,
+    printDepth(Depth), write(add(Diff)), write(' in actions'), nl,
     subset(Preconditions, Start),
     %write('Preconditions < Start'), nl,
-    write('Satisfying move = '), write(Name), nl,
+    printDepth(Depth), write('Satisfying move = '), write(Name), nl,
     change_state(Start, Actions, Child_state),
-    write('Child_state = '), print_stack(Child_state), nl,
+    printDepth(Depth), write('Child_state = '), print_stack(Child_state), nl,
     not(member_state(Child_state, Been_list)),
-    write('Child_state not in been list'), nl, 
+    printDepth(Depth), write('Child_state not in been list'), nl, 
     stack(Child_state, Been_list, New_been_list),
-    me_plan(Child_state, Goal, New_been_list, Plan, End_state),!.	%just find first plan
+    me_plan(Child_state, Goal, New_been_list, Plan, End_state, New_depth),!.	%just find first plan
 
 %this is the case where we pick a move to achieve a predicate in the goal
 %but not satisfied in the current state, and we lack at least one precondition to take that move
 %in this case plan from the current state to the preconditions of that move
 %then plan from the resulting state to the current goal, 
 %then append the first plan with the second plan, with the original move in between.
-me_plan(Start, Goal, Been_list, Plan, End_state) :-
+me_plan(Start, Goal, Been_list, Plan, End_state, Depth) :-
+    New_depth is Depth + 1,
     %write('2--plan from ['), print_stack(Start), 
     %write('] to ['), print_stack(Goal), write(']'), nl,
     set_diff(Goal, Start, Diff_set),
@@ -50,21 +52,21 @@ me_plan(Start, Goal, Been_list, Plan, End_state) :-
     member(add(Diff), Actions), 		%more than one postcondition matching doesn't help
     %assume subset(Preconditions, Start) is false
     not(subset(Preconditions, Start)),
-    write('2--preconditions '), print_stack(Preconditions), 
+    printDepth(Depth), write('2--preconditions '), print_stack(Preconditions), 
     write(' not a subset of start '), print_stack(Start), nl,
-    me_plan(Start, Preconditions, Been_list, Sub_plan, Sub_end_state),!,	%just find first plan
+    me_plan(Start, Preconditions, Been_list, Sub_plan, Sub_end_state, New_depth),!,	%just find first plan
     %write('2--Diff_set = '), print_stack(Diff_set), nl,
-    write('2--Sub_plan = '), print_stack(Sub_plan), nl,
-    write('2--Sub_end_state = '), print_stack(Sub_end_state), nl,
+    printDepth(Depth), write('2--Sub_plan = '), print_stack(Sub_plan), nl,
+    printDepth(Depth), write('2--Sub_end_state = '), print_stack(Sub_end_state), nl,
     change_state(Sub_end_state, Actions, Child_state),
-    write('2--Child_state = '), print_stack(Child_state), nl,
+    printDepth(Depth), write('2--Child_state = '), print_stack(Child_state), nl,
     stack(Child_state, Been_list, New_been_list),
     append(Sub_plan, [Name], Pre_moves),
-    write('2--Pre_moves = '), print_stack(Pre_moves), nl,
-    me_plan(Child_state, Goal, New_been_list, Post_moves, End_state),!,	%just find first plan
-    write('2--Post_moves = '), print_stack(Post_moves), nl,
+    printDepth(Depth), write('2--Pre_moves = '), print_stack(Pre_moves), nl,
+    me_plan(Child_state, Goal, New_been_list, Post_moves, End_state, New_depth),!,	%just find first plan
+    printDepth(Depth), write('2--Post_moves = '), print_stack(Post_moves), nl,
     append(Pre_moves, Post_moves, Plan),
-    write('2--append '), print_stack(Pre_moves), 
+    printDepth(Depth), write('2--append '), print_stack(Pre_moves), 
     write(' and '), print_stack(Post_moves), nl.
 
 %print a list as [1, 2, 3, 4], i.e comma delimited and surrounded by brackets
@@ -76,6 +78,11 @@ print_stack_rec(S) :- 	stack(E, [R|Rest], S), !,	%print a non-final element
 print_stack_rec(S) :- 	stack(E, [], S), 	%print a final element
 		 		write(E).
 
+printDepth(0) :- !. 
+printDepth(Depth) :- 
+	Depth_minus_one is (Depth - 1),
+	write('  '),
+	printDepth(Depth_minus_one).
 
 % plan(Start,     % the start state
 %      Goal,	  % the goal state
@@ -96,7 +103,7 @@ move(putdown(X), [holding(X)],
 move(stack(X, Y), [holding(X), clear(Y)],
 		  [del(holding(X)), del(clear(Y)), add(handempty), add(on(X, Y)), add(clear(X))]).
 
-go(S, G, P) :- me_plan(S, G, [S], P, _).
+go(S, G, P) :- me_plan(S, G, [S], P, _, 0).
 
 test4(P) :-
 	go([ontable(c), on(b, c), on(a,b), clear(a)],
