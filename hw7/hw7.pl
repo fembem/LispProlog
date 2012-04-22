648eb6e123ea11c77184f0c36847d33b115caec8
Leo <leo.decandia@gmail.com>
2012-04-21 21:15:01 -1000

    added code for Preconditions subplanning.

--- hw7/hw7.pl
+++ hw7/hw7.pl
@@ -21,22 +21,36 @@ me_plan(Start, Goal, _, Plan, Start) :- 	%when we reach the goal the final end s
 
 me_plan(Start, Goal, Been_list, [Name|Plan], End_state) :-
     set_diff(Goal, Start, Diff_set),
-    write('Diff_set = '), print_stack(Diff_set), nl,
+    %write('Diff_set = '), print_stack(Diff_set), nl,
     member(Diff, Diff_set),				%just find first predicate we are missing
-    write('Diff = '), write(Diff), nl,
+    %write('Diff = '), write(Diff), nl,
     move(Name, Preconditions, Actions),
-    write('Name = '), write(Name), nl,
+    %write('Name = '), write(Name), nl,
     member(add(Diff), Actions), 		%more than one postcondition matching doesn't help
-    write('add(Diff) in actions'), nl,
+    %write('add(Diff) in actions'), nl,
     subset(Preconditions, Start),
-    write('Preconditions < Start'), nl,
+    %write('Preconditions < Start'), nl,
     change_state(Start, Actions, Child_state),
-    write('Child_state = '), print_stack(Child_state), nl,
+    %write('Child_state = '), print_stack(Child_state), nl,
     not(member_state(Child_state, Been_list)),
-    write('is new state'), nl,
+    %write('is new state'), nl,
     stack(Child_state, Been_list, New_been_list),
     me_plan(Child_state, Goal, New_been_list, Plan, End_state),!.	%just find first plan
 
+me_plan(Start, Goal, Been_list, Plan, End_state) :-
+    set_diff(Goal, Start, Diff_set),
+    %write('2)Diff_set = '), print_stack(Diff_set), nl,
+    member(Diff, Diff_set),				%just find first predicate we are missing
+    %write('2)Diff = '), write(Diff), nl,
+    move(Name, Preconditions, Actions),
+    %write('2)Name = '), write(Name), nl,
+    member(add(Diff), Actions), 		%more than one postcondition matching doesn't help
+    me_plan(Start, Preconditions, Been_list, Sub_plan, End_state),!,	%just find first plan
+    change_state(End_state, Actions, Child_state),
+    stack(Child_state, Been_list, New_been_list),
+    append(Sub_plan, [Name], Pre_moves),
+    me_plan(Child_state, Goal, New_been_list, Post_moves, End_state),!,	%just find first plan
+    append(Pre_moves, Post_moves, Plan).
 
 print_stack(S) :- 	empty_stack(S).
 print_stack(S) :- 	stack(E, Rest, S), 
