202e6e4a8246e57d01610ef003a888722afdd699
Leo <leo.decandia@gmail.com>
2012-04-22 08:11:24 -1000

    improved behavior and more logging.

--- hw7/hw7.pl
+++ hw7/hw7.pl
@@ -14,58 +14,65 @@
 
 :- [planner].
 
-me_plan(Start, Goal, _, Plan, Start) :- 	%when we reach the goal the final end state will be our present 'start'
-	subset(Goal, Start), 
-	write('plan is'), nl,
-	print_stack(Plan).
+me_plan(Start, Goal, _, [], Start) :- 	%when we reach the goal the final end state will be our present 'start'
+	subset(Goal, Start), !, %only look for the first solution
+	write('Goal '), print_stack(Goal), 
+    write('a subset of Start '), print_stack(Start), nl.
 
 me_plan(Start, Goal, Been_list, [Name|Plan], End_state) :-
+    write('plan from '), print_stack(Start), 
+    write(' to '), print_stack(Goal), nl,
     set_diff(Goal, Start, Diff_set),
     write('Diff_set = '), print_stack(Diff_set), nl,
     member(Diff, Diff_set),				%just find first predicate we are missing
-    write('Diff = '), write(Diff), nl,
+    %write('Diff = '), write(Diff), nl,
     move(Name, Preconditions, Actions),
-    write('Name = '), write(Name), nl,
     member(add(Diff), Actions), 		%more than one postcondition matching doesn't help
-    write('add(Diff) in actions'), nl,
+    write(add(Diff)), write(' in actions'), nl,
     subset(Preconditions, Start),
-    write('Preconditions < Start'), nl,
-    write('Name = '), write(Name), nl,
+    %write('Preconditions < Start'), nl,
+    write('Satisfying move = '), write(Name), nl,
     change_state(Start, Actions, Child_state),
     write('Child_state = '), print_stack(Child_state), nl,
     not(member_state(Child_state, Been_list)),
-    write('is new state'), nl,
+    write('Child_state not in been list'), nl, 
     stack(Child_state, Been_list, New_been_list),
     me_plan(Child_state, Goal, New_been_list, Plan, End_state),!.	%just find first plan
 
 me_plan(Start, Goal, Been_list, Plan, End_state) :-
+    %write('2--plan from ['), print_stack(Start), 
+    %write('] to ['), print_stack(Goal), write(']'), nl,
     set_diff(Goal, Start, Diff_set),
-    write('2--Diff_set = '), print_stack(Diff_set), nl,
+    %write('2--Diff_set = '), print_stack(Diff_set), nl,
     member(Diff, Diff_set),				%just find first predicate we are missing
-    write('2--Diff = '), write(Diff), nl,
+    %write('2--Diff = '), write(Diff), nl,
     move(Name, Preconditions, Actions),
-    write('2--Name = '), write(Name), nl,
     member(add(Diff), Actions), 		%more than one postcondition matching doesn't help
     %assume subset(Preconditions, Start) is false
     not(subset(Preconditions, Start)),
-    write('2--preconditions ['), print_stack(Preconditions), 
-    write('] not a subset of start ['), print_stack(Start), write(']'), nl,
+    write('2--preconditions '), print_stack(Preconditions), 
+    write(' not a subset of start '), print_stack(Start), nl,
     me_plan(Start, Preconditions, Been_list, Sub_plan, End_state),!,	%just find first plan
-    write('2--Diff_set = '), print_stack(Diff_set), nl,
-    change_state(End_state, Actions, Child_state),
+    %write('2--Diff_set = '), print_stack(Diff_set), nl,
     write('2--Sub_plan = '), print_stack(Sub_plan), nl,
+    change_state(End_state, Actions, Child_state),
+    write('2--Child_state = '), print_stack(Child_state), nl,
     stack(Child_state, Been_list, New_been_list),
     append(Sub_plan, [Name], Pre_moves),
     me_plan(Child_state, Goal, New_been_list, Post_moves, End_state),!,	%just find first plan
     append(Pre_moves, Post_moves, Plan),
     write('2--append ['), print_stack(Pre_moves), 
-    write('] and ['), print_stack(Post_moves), write(']'), nl.
+    write(' and '), print_stack(Post_moves), nl.
 
-print_stack(S) :- 	empty_stack(S).
-print_stack(S) :- 	stack(E, Rest, S), 
+print_stack(S) :- write('['), print_stack_rec(S).
+print_stack_rec(S) :- 	empty_stack(S), write(']').
+print_stack_rec(S) :- 	stack(E, [R|Rest], S), !,
+		 		write(E), write(', '),
+		 		print_stack_rec([R|Rest]).
+print_stack_rec(S) :- 	stack(E, Rest, S), 
 		 		write(E),
-		 		write(', '),
-		 		print_stack(Rest).
+		 		print_stack_rec(Rest).
+
 
 % plan(Start,     % the start state
 %      Goal,	  % the goal state
@@ -87,6 +94,11 @@ move(stack(X, Y), [holding(X), clear(Y)],
 		  [del(holding(X)), del(clear(Y)), add(handempty), add(on(X, Y)), add(clear(X))]).
 
 go(S, G, P) :- me_plan(S, G, [S], P, _).
+
+test4(P) :-
+	go([ontable(c), on(b, c), on(a,b), clear(a)],
+	   [on(a,b), on(b, c)], P).
+
 test0(P) :-
 	go([holding(a), ontable(c), on(b, c), clear(b)],
 	   [on(a,b), on(b, c)], P).
